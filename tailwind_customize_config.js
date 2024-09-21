86b39a30c356fde58bbcd5e1c52b00be816619a2
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-09-21 21:14:36 +0600

    Initial project setup done

---
+++ tailwind_customize_config.js
@@ -0,0 +1,11 @@
+// customize configuration with own design tokens
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        }
+      }
+    }
+  }
